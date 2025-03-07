7e56dcdee7d3d91f490a5b07b02769a77d65c393
llglh <2633830336@qq.com>
2025-03-07 11:42:26 +0800

    实现在窗口内移动

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
-from PySide6.QtWidgets import QWidget,QApplication,QLabel,QGraphicsOpacityEffect
-from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QWidget,QApplication,QLabel,QGraphicsOpacityEffect,QHBoxLayout,QPushButton
+from PySide6.QtCore import Qt,QPoint
+from PySide6.QtGui import QIcon,QPixmap,QMouseEvent,QPaintEvent,QPainter
 class Pet(QWidget):
   def __init__(self):
     super().__init__()
-    self.init_ui() 
-
+    self.init_ui()
   def init_ui(self):
-      """初始化界面"""
-      self.setWindowTitle("Desktop Pet")
-      self.setWindowFlags(Qt.WindowType.FramelessWindowHint | 
-                          Qt.WindowType.WindowStaysOnTopHint |
-                          Qt.WindowType.Tool)
-      self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
-      self.setAttribute(Qt.WidgetAttribute.WA_AcceptTouchEvents, True)
-       # 设置初始大小和位置
-      self.resize(128, 128)
-      screen_geo = QApplication.primaryScreen().geometry()
-      self.move(screen_geo.width()-150, screen_geo.height()-150)
-      
-      # 创建显示标签
-      self.label = QLabel(self)
-      self.label.setGeometry(0, 0, 128, 128)
-      self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-      self.label.setText("111111")
-      # 添加半透明效果
-      opacity_effect = QGraphicsOpacityEffect(self)
-      opacity_effect.setOpacity(0.9)
-      self.label.setGraphicsEffect(opacity_effect)
+      # 设置窗口标志，去掉标题栏
+      self.setWindowFlags(Qt.WindowType.Dialog | Qt.WindowType.FramelessWindowHint)
+      # 设置窗口背景透明
+      self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground, True)
+      self.setGeometry(300, 300, 300, 200)
+      self.layout_main=QHBoxLayout()
+      self.img_label=MyLabel()
+      self.layout_main.addWidget(self.img_label)
+      self.setLayout(self.layout_main)
+
+class MyLabel(QLabel):
+    def __init__(self):
+        super().__init__()
+        self.img_main = QPixmap('img/shime1.png')
+        self.update()
+        self.pressedpos=QPoint()
+    def mousePressEvent(self, ev: QMouseEvent):
+        if ev.buttons() & Qt.MouseButton.LeftButton:
+          self.img_main.load('img/shime2.png')
+          self.update()
+          self.pressedpos.setX(ev.x())
+          self.pressedpos.setY(ev.y())
+    def paintEvent(self,ev:QPaintEvent):
+      with QPainter(self) as self.p:
+        self.p.drawPixmap(self.rect(), self.img_main)
+    def mouseMoveEvent(self, ev: QMouseEvent):
+      self.move(self.pos()+ev.pos()-self.pressedpos)
 
 if __name__ =='__main__':
   app=QApplication([])
