0db7d339736df453bdf7b8f4297d5387a2f1f750
llglh <2633830336@qq.com>
2025-03-07 11:58:35 +0800

    实现全局移动

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ class Pet(QWidget):
     self.init_ui()
   def init_ui(self):
       # 设置窗口标志，去掉标题栏
-      self.setWindowFlags(Qt.WindowType.Dialog | Qt.WindowType.FramelessWindowHint)
+      self.setWindowFlags(Qt.WindowType.Dialog | Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint)
       # 设置窗口背景透明
       self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground, True)
-      self.setGeometry(300, 300, 300, 200)
+      # self.setGeometry(300, 300, 300, 200)
+      self.showFullScreen()
       self.layout_main=QHBoxLayout()
       self.img_label=MyLabel()
       self.layout_main.addWidget(self.img_label)
@@ -20,19 +21,20 @@ class MyLabel(QLabel):
     def __init__(self):
         super().__init__()
         self.img_main = QPixmap('img/shime1.png')
+        self.setFixedSize(self.img_main.width(),self.img_main.height())
         self.update()
         self.pressedpos=QPoint()
     def mousePressEvent(self, ev: QMouseEvent):
         if ev.buttons() & Qt.MouseButton.LeftButton:
           self.img_main.load('img/shime2.png')
           self.update()
-          self.pressedpos.setX(ev.x())
-          self.pressedpos.setY(ev.y())
+          self.pressedpos.setX(ev.position().x())
+          self.pressedpos.setY(ev.position().y())
     def paintEvent(self,ev:QPaintEvent):
       with QPainter(self) as self.p:
         self.p.drawPixmap(self.rect(), self.img_main)
     def mouseMoveEvent(self, ev: QMouseEvent):
-      self.move(self.pos()+ev.pos()-self.pressedpos)
+      self.move(self.pos()+ev.position().toPoint()-self.pressedpos)
 
 if __name__ =='__main__':
   app=QApplication([])
