acaa26623c708e9538b8379ee3787e9688f36c3d
llglh <2633830336@qq.com>
2025-04-17 23:49:57 +0800

    test

--- pet_ds/config.py
+++ pet_ds/config.py
@@ -3,15 +3,17 @@ from pydantic import BaseModel
 
 class Config(BaseModel):
     # LLM 密钥
-    api_key: str = ""
+    api_key: str = (
+        "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"
+    )
     # LLM url
     llm_url: str = "https://openrouter.ai/api/v1/chat/completions"
     # 模型
     model: str = "deepseek/deepseek-chat-v3-0324:free"
     # mcp server 路径
-    mcp_server_path: str = "E:\\work\\vscode\\pet-ds\\pet_ds\\mcp\\server\\weather.py"
+    mcp_server_path: str = "D:/junior-work/junior/pet-ds/pet_ds/mcp/server/weather.py"
     # 代理
-    proxy: str = None
+    proxy: str = "http://127.0.0.1:7890"
 
 
 conf = Config()
